adfebd5f397d5eafdbbe7aeef1659849819d0229
Dorota Przeniosło <d.przenioslo@centrala.kaczmarski.pl>
2017-06-20 11:27:02 +0200

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
   render() {
     return (
-      <h1>hello sophie</h1>
+      <h1>hello!!!</h1>
     )
   }
 }
--- tools/server.js
+++ tools/server.js
@@ -3,29 +3,35 @@ import webpack from 'webpack';
 import path from 'path';  
 import config from '../webpack.config.dev';  
 import open from 'open';  
-import favicon from 'serve-favicon';
+import socket from 'socket.io'  
+import { Server } from 'http' 
 
-/* eslint-disable no-console */
 
 const port = 3000;  
-const app = express();  
-const compiler = webpack(config);
+const app = express()  
+const server = Server(app)  
+const compiler = webpack(config)  
+const io = socket(server) 
+
 
 app.use(require('webpack-dev-middleware')(compiler, {  
-  noInfo: true,
-  publicPath: config.output.publicPath
+  noInfo: true, publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));  
+app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {  
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.use('*', function(req, res) {  
+ res.sendFile(path.join(__dirname, '../src/index.html')); 
 });
 
-app.listen(port, function(err) {  
-  if (err) {
-    console.log(err);
-  } else {
-    open(`http://localhost:${port}`);
-  }
-});
+io.on('connection', function(socket) {  
+  console.log('a user connected')
+})
+
+server.listen(port, function(err) {  
+  if (err) { 
+    console.log('error', err); 
+   } else { 
+      open(`http://localhost:${port}`);
+   }
+ });
